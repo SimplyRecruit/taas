a3555bb5460afc023e4bf83889ee40a158199693
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-01-14 00:54:03 +0300

    feat: Create timetrack table

---
+++ server/resources/TimeTrack/Entity.ts
@@ -0,0 +1,39 @@
+import EntityBase from "../../EntityBase"
+import { Entity, Column, BaseEntity, PrimaryColumn, JoinColumn, ManyToOne } from "typeorm"
+import { CustomerEntity } from "../Customer/Entity"
+import { ProjectEntity } from "../Project/Entity"
+import { ResourceEntity } from "../Resource/Entity"
+
+@Entity("time_track")
+export class TimeTrackEntity extends EntityBase {
+
+    @ManyToOne(() => ResourceEntity)
+    @JoinColumn()
+    resource: ResourceEntity
+
+    @ManyToOne(() => CustomerEntity)
+    @JoinColumn()
+    customer: CustomerEntity
+
+    @ManyToOne(() => ProjectEntity)
+    @JoinColumn()
+    project: ProjectEntity
+
+
+    @Column()
+    date: Date
+
+    @Column({ type: "decimal", precision: 1 })
+    hour: number
+
+    @Column()
+    description: string
+
+    @Column()
+    billable: boolean
+
+    @Column()
+    ticketNo: string
+
+
+}
