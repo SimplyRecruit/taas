6280ab0388986785a8bcc38b0166fb725cb0fbb8
dogkan-spiky <dogkan@spiky.ai>
2023-01-26 00:19:38 +0300

    BE-1 refactor: Make email template ids language types lowercase

--- server/common/Config.ts
+++ server/common/Config.ts
@@ -1,9 +1,9 @@
 export const EMAIL_TEMPLATE_IDs: any = {
     ResetPasswordEmailTemplate: {
-        EN: "d-808240c1787147a183cab644da1360ad"
+        en: "d-808240c1787147a183cab644da1360ad"
     },
     VerificationEmailTemplate: {
-        EN: "d-37ccd7664e5242319a3e979e30d788d1",
-        TR: "d-ef656d97a1d6440b82f68f7c5686c95f"
+        en: "d-37ccd7664e5242319a3e979e30d788d1",
+        tr: "d-ef656d97a1d6440b82f68f7c5686c95f"
     }
 }
