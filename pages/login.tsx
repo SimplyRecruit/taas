307d57de5247fdf13f83088c995421fac4f9fb0f
dogkan-spiky <dogkan@spiky.ai>
2023-01-27 00:21:34 +0300

    FE-1 feat: Add background image to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Typography, Space, Col, Row } from 'antd';
+import { Form, Input, Button, Typography, Space, Col, Row, Checkbox, Card } from 'antd';
 import Link from 'next/link';
 import React, { useState } from 'react';
 
@@ -21,44 +21,46 @@ const LoginPage = () => {
     };
 
     return (
-        <Row justify="space-around" align="middle" style={{ minHeight: '100vh' }}>
-            <Col >
-                <Typography.Title level={2}>Sign In</Typography.Title>
-                <Space align="start" size="small" />
-                <Form
-                    form={form}
-                    name="basic"
-                    layout="vertical"
-                    initialValues={formData}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                >
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[{ required: true, message: 'Please enter your email!' }]}
+        <Row justify="space-around" align="middle" style={{ minHeight: '100vh', backgroundImage: 'url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minWidth: 250 }} >
+                <Card>
+                    <Typography.Title level={2}>Sign In</Typography.Title>
+                    <Space align="start" size="small" />
+                    <Form
+                        form={form}
+                        name="basic"
+                        layout="vertical"
+                        style={{ maxWidth: 250 }}
+                        initialValues={formData}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
                     >
-                        <Input type="email" placeholder="Enter your email" />
-                    </Form.Item>
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please enter your password!' }]}
-                    >
-                        <Input.Password placeholder="Enter your password" />
-                    </Form.Item>
-                    <Form.Item >
-                        <Button block htmlType="submit" >Submit</Button>
-                    </Form.Item>
-                    <Link passHref href="/register">
-                        <Button
-                            type="ghost"
+                        <Form.Item
+                            name="email"
+                            rules={[{ required: true, message: 'Please enter your email!' }]}
                         >
-                            Forgot your password?
-                        </Button>
-                    </Link>
-                </Form>
-            </Col>
+                            <Input type="email" placeholder="Enter your email" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: 'Please enter your password!' }]}
+                        >
+                            <Input.Password placeholder="Enter your password" />
+                        </Form.Item>
+                        <Form.Item >
+                            <Button block htmlType="submit" >Submit</Button>
+                        </Form.Item>
+                        <Link passHref href="/register">
+                            <Button
+                                style={{ padding: 0 }}
+                                type="ghost"
+                            >
+                                Forgot password?
+                            </Button>
+                        </Link>
+                    </Form>
+                </Card>
+            </div>
         </Row>
     );
 };
