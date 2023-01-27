bfe582db82da915b617f2d3c9b82f02ffa4e3c59
dogkan-spiky <dogkan@spiky.ai>
2023-01-27 22:26:35 +0300

    FE-1 fix: Set maxWidth to login card

--- pages/login.tsx
+++ pages/login.tsx
@@ -21,47 +21,46 @@ const LoginPage = () => {
     };
 
     return (
-        <Row justify="space-around" align="middle" style={{ minHeight: '100vh', backgroundImage: 'url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", minWidth: 250 }} >
-                <Card>
-                    <Typography.Title level={2}>Sign In</Typography.Title>
-                    <Space align="start" size="small" />
-                    <Form
-                        form={form}
-                        name="basic"
-                        layout="vertical"
-                        style={{ maxWidth: 250 }}
-                        initialValues={formData}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
+        <div className='center-column' style={{ backgroundImage: 'url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <Card className='elevation' style={{ width: '100%', maxWidth: 300 }}>
+                <Typography.Title level={2}>Sign In</Typography.Title>
+                <Space align="start" size="small" />
+                <Form
+                    form={form}
+                    name="basic"
+                    layout="vertical"
+                    style={{ width: '100%' }}
+                    initialValues={formData}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[{ required: true, message: 'Please enter your email!' }]}
                     >
-                        <Form.Item
-                            name="email"
-                            rules={[{ required: true, message: 'Please enter your email!' }]}
+                        <Input type="email" placeholder="Enter your email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please enter your password!' }]}
+                    >
+                        <Input.Password placeholder="Enter your password" />
+                    </Form.Item>
+                    <Form.Item >
+                        <Button block htmlType="submit">Submit</Button>
+                    </Form.Item>
+                    <Link passHref href="/forgot-password">
+                        <Button
+                            style={{ padding: 0 }}
+                            type="ghost"
                         >
-                            <Input type="email" placeholder="Enter your email" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: 'Please enter your password!' }]}
-                        >
-                            <Input.Password placeholder="Enter your password" />
-                        </Form.Item>
-                        <Form.Item >
-                            <Button block htmlType="submit" >Submit</Button>
-                        </Form.Item>
-                        <Link passHref href="/register">
-                            <Button
-                                style={{ padding: 0 }}
-                                type="ghost"
-                            >
-                                Forgot password?
-                            </Button>
-                        </Link>
-                    </Form>
-                </Card>
-            </div>
-        </Row>
+                            Forgot password?
+                        </Button>
+                    </Link>
+                </Form>
+            </Card>
+
+        </div>
     );
 };
 
