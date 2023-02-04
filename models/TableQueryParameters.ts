0ce6a11023c3e168f427aaac80695ce9f07e1713
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-02-04 19:25:45 +0300

    feat: Add a constructor for TableQueryParameters

--- models/TableQueryParameters.ts
+++ models/TableQueryParameters.ts
@@ -3,6 +3,13 @@ import { IsInt, IsOptional, IsPositive, Matches, Min } from "class-validator";
 const sortByRegex = /^(\~|\-)([a-zA-Z0-9_]*)$/
 
 export default class TableQueryParameters {
+
+    constructor(sortBy?: string[], limit?: number, offset?: number) {
+        this.sortBy = sortBy
+        this.limit = limit
+        this.offset = offset
+    }
+
     @IsOptional()
     @Matches(sortByRegex, { each: true })
     private sortBy?: string[]
