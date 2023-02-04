eca1f9676c5d7e57d319fd7f607148f0257a1e9d
dogkan-spiky <dogkan@spiky.ai>
2023-02-04 20:44:11 +0300

    FE-6 feat: Make TrackerList bottom borders square

--- src/pages/tracker/components/TrackerList/index.tsx
+++ src/pages/tracker/components/TrackerList/index.tsx
@@ -12,9 +12,8 @@ export default function TrackerList({ data }: RenderProps) {
         <>
             <List
                 className={styles.wrapper}
-                style={{ marginBottom: 24 }}
+                style={{ marginBottom: 24, borderRadius: "8px 8px 2px 2px", borderBottom: "4px solid #c0caea" }}
                 size="large"
-                footer={<div> </div>}
                 bordered
                 header={<Typography.Text type="secondary" >Today</Typography.Text>}
                 dataSource={data}
