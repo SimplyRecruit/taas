6332cbcb60d175ca87eddd634b8c1f184e58ac8e
dogkan-spiky <dogkan@spiky.ai>
2023-02-11 02:10:46 +0300

    FE-7 feat: Add projects page

---
+++ src/pages/projects/data.ts
@@ -0,0 +1,18 @@
+export const dummyData = [
+  {
+    key: 0,
+    name: 'Kurulum',
+    client: 'A101',
+    startDate: '22.05.2000',
+    role: 'Developer',
+    status: 'active',
+  },
+  {
+    key: 1,
+    name: 'Bakım',
+    client: 'BİM',
+    startDate: '22.05.2000',
+    role: 'Designer',
+    status: 'inactive',
+  },
+]
---
+++ src/pages/projects/index.page.tsx
@@ -0,0 +1,136 @@
+import { useState } from 'react'
+import { Button, Input, Modal, Select, Space, Table } from 'antd'
+import { FiEdit2 } from 'react-icons/fi'
+import { SearchOutlined } from '@ant-design/icons'
+import ChangeRateModal from '@/pages/team/components/ChangeRateModal'
+import { dummyData } from '@/pages/projects/data'
+
+export default function ProjectsPage() {
+  const roles = ['Admin', 'Manager', 'End-user']
+  const actionColumnWidth = 60
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text: string, record: any) => (
+        <span
+          style={{
+            textDecoration:
+              record.status === 'inactive' ? 'line-through' : 'none',
+          }}
+        >
+          {text}
+        </span>
+      ),
+    },
+    {
+      title: 'Client',
+      dataIndex: 'client',
+      key: 'client',
+    },
+    {
+      title: 'Start Date',
+      dataIndex: 'startDate',
+      key: 'startDate',
+    },
+    {
+      title: '',
+      key: 'action',
+      width: actionColumnWidth,
+      render: () => (
+        <span>
+          <FiEdit2 type="edit" style={{ cursor: 'pointer' }} />
+        </span>
+      ),
+    },
+  ]
+
+  const [modalOpen, setModalOpen] = useState(false)
+  const [searchText, setSearchText] = useState('')
+  const [filteredData, setFilteredData] = useState(dummyData)
+  const [data, setData] = useState(dummyData)
+  const [selectedStatus, setSelectedStatus] = useState('all')
+  const [currentRecord, setCurrentRecord] = useState(
+    dummyData[0] ? dummyData[0] : null
+  )
+
+  const handleStatusChange = (value: string) => {
+    setSelectedStatus(value)
+    filterData(value, searchText)
+  }
+
+  const handleSearch = (value: string) => {
+    setSearchText(value)
+    filterData(selectedStatus, value)
+  }
+
+  const filterData = (status: string, search: string) => {
+    let filtered = data
+    if (status !== 'all') {
+      filtered = filtered.filter(item => item.status === status)
+    }
+    if (search) {
+      filtered = filtered.filter(item =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      )
+    }
+    setFilteredData(filtered)
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: 20,
+        }}
+      >
+        <Space size="small">
+          <Select
+            defaultValue="all"
+            style={{ width: 120 }}
+            onChange={handleStatusChange}
+          >
+            <Select.Option value="all">All</Select.Option>
+            <Select.Option value="active">Active</Select.Option>
+            <Select.Option value="inactive">Inactive</Select.Option>
+          </Select>
+          <Input
+            prefix={<SearchOutlined />}
+            allowClear
+            placeholder="Search Name"
+            value={searchText}
+            onChange={e => handleSearch(e.target.value)}
+            style={{ width: 200 }}
+          />
+        </Space>
+        <Button type="primary" onClick={() => setModalOpen(true)}>
+          Add Project
+        </Button>
+      </div>
+      <Table
+        columns={columns}
+        dataSource={filteredData}
+        pagination={{
+          position: ['bottomCenter'],
+          responsive: true,
+          showQuickJumper: false,
+          showLessItems: true,
+          showTotal: total => `Total ${total} clients`,
+          showSizeChanger: false,
+        }}
+      />
+      <Modal
+        title="Add Project"
+        open={modalOpen}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+      >
+        {/* Add form inputs to capture new client data */}
+      </Modal>
+    </div>
+  )
+}
