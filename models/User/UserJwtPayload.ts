61a6c1b1009a01731b243e0d1b8bf70eb5711834
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-02-11 18:01:35 +0300

    BE-2 feat: Check if user is confirmed and active in auth check

--- models/User/UserJwtPayload.ts
+++ models/User/UserJwtPayload.ts
@@ -1,9 +1,12 @@
 import Model from 'models/Model'
+import UserStatus from 'models/User/UserStatus'
 import UserRole from './UserRole'
 
 export default class UserJwtPayload extends Model {
   id: string
   role: UserRole
+  status: UserStatus
+  active: boolean
   iat: number
   exp: number
 }
