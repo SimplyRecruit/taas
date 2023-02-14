37c660e32356443450875957c36d89073ea4bc87
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-02-14 20:24:02 +0300

    conf: Add "create namespace" function

--- generate-locale-schemas.js
+++ generate-locale-schemas.js
@@ -1,43 +1,83 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-console.log(process.argv)
-const { 2: namespace } = process.argv
-if (!namespace)
-  throw 'Command usage: npm run generate-locale-schemas -- <namespace>'
 const jsonSchemaGenerator = require('json-schema-generator')
 const fs = require('fs')
 const path = require('path')
-const json = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, `./public/locales/en/${namespace}.json`),
-    {
-      encoding: 'utf-8',
-    }
-  )
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const foldersOfLocales = fs.readdirSync(
+  path.join(__dirname, './public/locales'),
+  {
+    withFileTypes: true,
+  }
 )
-const schemaObj = jsonSchemaGenerator(json)
-schemaObj['additionalProperties'] = false
-fs.writeFileSync(
-  path.join(__dirname, `./public/locales/schemas/${namespace}.json`),
-  JSON.stringify(schemaObj, null, 4),
-  { encoding: 'utf-8', flag: 'w' }
-)
-fs.readdirSync(path.join(__dirname, './public/locales'), {
-  withFileTypes: true,
-}).forEach(f => {
-  if (f.name !== 'schemas') {
-    const langFile = JSON.parse(
+function generateSchemas() {
+  readline.question('\nPlease enter namespace:\n', namespace => {
+    readline.close()
+    const json = JSON.parse(
       fs.readFileSync(
-        path.join(__dirname, `./public/locales/${f.name}/${namespace}.json`),
+        path.join(__dirname, `./public/locales/en/${namespace}.json`),
         {
           encoding: 'utf-8',
         }
       )
     )
-    langFile['$schema'] = `../schemas/${namespace}.json`
+    const schemaObj = jsonSchemaGenerator(json)
+    schemaObj['additionalProperties'] = false
     fs.writeFileSync(
-      path.join(__dirname, `./public/locales/${f.name}/${namespace}.json`),
-      JSON.stringify(langFile, null, 4),
+      path.join(__dirname, `./public/locales/schemas/${namespace}.json`),
+      JSON.stringify(schemaObj, null, 4),
       { encoding: 'utf-8', flag: 'w' }
     )
-  }
-})
+    foldersOfLocales.forEach(f => {
+      if (f.name !== 'schemas') {
+        const langFileObject = JSON.parse(
+          fs.readFileSync(
+            path.join(
+              __dirname,
+              `./public/locales/${f.name}/${namespace}.json`
+            ),
+            {
+              encoding: 'utf-8',
+            }
+          )
+        )
+        langFileObject['$schema'] = `../schemas/${namespace}.json`
+        fs.writeFileSync(
+          path.join(__dirname, `./public/locales/${f.name}/${namespace}.json`),
+          JSON.stringify(langFileObject, null, 4),
+          { encoding: 'utf-8', flag: 'w' }
+        )
+      }
+    })
+    console.info('\nDone')
+  })
+}
+
+function createNamespace() {
+  readline.question('\nPlease enter a name for the namespace:\n', namespace => {
+    readline.close()
+    const schemaObj = jsonSchemaGenerator({})
+    schemaObj['additionalProperties'] = false
+    fs.writeFileSync(
+      path.join(__dirname, `./public/locales/schemas/${namespace}.json`),
+      JSON.stringify(schemaObj, null, 4),
+      { encoding: 'utf-8', flag: 'w' }
+    )
+    foldersOfLocales.forEach(f => {
+      if (f.name !== 'schemas') {
+        const langFileObject = { $schema: `../schemas/${namespace}.json` }
+        fs.writeFileSync(
+          path.join(__dirname, `./public/locales/${f.name}/${namespace}.json`),
+          JSON.stringify(langFileObject, null, 4),
+          { encoding: 'utf-8', flag: 'w' }
+        )
+      }
+    })
+    console.info('\nDone')
+  })
+}
+
+module.exports = { generateSchemas, createNamespace }
