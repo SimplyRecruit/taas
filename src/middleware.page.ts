c40f60ea910abcc542b252e7b96de8899acede13
Ahmet Berk TAŞ <63806963+Apperk274@users.noreply.github.com>
2023-02-15 18:44:43 +0300

    conf: Detect language or use cookie for language

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -3,8 +3,34 @@ import type { NextRequest } from 'next/server'
 import { checkAuthentication } from '@/auth/utils/AuthUtil'
 import { Route } from '@/constants'
 import cookieKeys from '@/constants/cookie-keys'
+import { isLanguage } from 'models/Language'
+
+const PUBLIC_FILE = /\.(.*)$/
 
 export async function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith('/_next') ||
+    request.nextUrl.pathname.includes('/api/') ||
+    PUBLIC_FILE.test(request.nextUrl.pathname)
+  ) {
+    return
+  }
+
+  if (request.nextUrl.locale === 'default') {
+    const detectedLanguage =
+      request.headers.get('Accept-Language')?.split(',')[0].substring(0, 2) ??
+      ''
+    const locale =
+      request.cookies.get('NEXT_LOCALE')?.value ??
+      (isLanguage(detectedLanguage) ? detectedLanguage : 'en')
+    return NextResponse.redirect(
+      new URL(
+        `/${locale}${request.nextUrl.pathname}${request.nextUrl.search}`,
+        request.url
+      )
+    )
+  }
+
   const url = request.nextUrl.clone()
   if (url.pathname === Route.Logout) {
     url.pathname = Route.Login
@@ -25,7 +51,8 @@ export async function middleware(request: NextRequest) {
       if (user) {
         response.cookies.set(
           cookieKeys.COOKIE_USER_OBJECT,
-          JSON.stringify(user)
+          JSON.stringify(user),
+          { path: '/' }
         )
       }
       return response
@@ -33,7 +60,11 @@ export async function middleware(request: NextRequest) {
     case 'app': {
       url.pathname = Route.DashBoard
       const response = NextResponse.redirect(url)
-      response.cookies.set(cookieKeys.COOKIE_USER_OBJECT, JSON.stringify(user))
+      response.cookies.set(
+        cookieKeys.COOKIE_USER_OBJECT,
+        JSON.stringify(user),
+        { path: '/' }
+      )
       return response
     }
     case 'login': {
@@ -41,8 +72,4 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(url)
     }
   }
-}
-
-export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 }
