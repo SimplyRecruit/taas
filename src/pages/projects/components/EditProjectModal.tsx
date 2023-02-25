f41b1f8ef0132cfdfbf93810bef8d3fcb3b01896
dogkan-spiky <dogkan@spiky.ai>
2023-02-25 20:45:04 +0300

    TAAS-4 feat: Change modal with drawer. Set Form template

--- src/pages/projects/components/EditProjectModal.tsx
+++ src/pages/projects/components/EditProjectModal.tsx
@@ -1,76 +1,193 @@
-import { DatePicker, Dropdown, Modal, Select, Space } from 'antd'
-import { Input } from 'antd'
-import dayjs from 'dayjs'
-import { Project } from 'models'
-import { useState } from 'react'
+import moment from 'dayjs'
+import {
+  Input,
+  Form,
+  Row,
+  Col,
+  Drawer,
+  Button,
+  Space,
+  DatePicker,
+  Radio,
+  Select,
+} from 'antd'
+import {
+  Project,
+  ProjectCreateBody,
+  ProjectUpdateBody,
+} from 'models'
+import { CloseOutlined } from '@ant-design/icons'
+import { momentToDate } from '@/util'
+import { DEFAULT_DATE_FORMAT } from '@/constants'
+import useApi from '@/services/useApi'
+import { useEffect } from 'react'
 
 interface RenderProps {
   open: boolean
-  project: Project | null
-  onAdd: (project: Project) => void
-  onUpdate: (project: Project) => void
+  value: Project | null
+  onAdd: (newProject: Project) => void
+  onUpdate: (updatedProject: Project) => void
   onCancel: () => void
 }
-export default function EditProjectModal({
+export default function EditProjectDrawer({
   open,
-  project,
   onAdd,
+  value,
   onUpdate,
   onCancel,
 }: RenderProps) {
-  const [name, setName] = useState(project ? project.name : '')
-  const [client, setClient] = useState(project ? project.client : '')
-  const [startDate, setStartDate] = useState(
-    project ? project.startDate : dayjs()
+  const [form] = Form.useForm<ProjectCreateBody | ProjectUpdateBody>()
+  const {
+    call,
+    data,
+    loading: loadingGetAll,
+    error,
+  } = useApi('client', 'getAll')
+  const { call: callCreate, loading: loadingCreate } = useApi(
+    'project',
+    'create'
   )
-  const onOk = () => {
-    project
-      ? onUpdate({
-          id: project.id,
-          name,
-          client,
-          startDate,
-          active: project.active,
-        })
-      : onAdd({
-          id: Math.random().toString(),
-          name,
-          client,
-          startDate,
-          active: true,
-        })
+  const { call: callUpdate, loading: loadingUpdate } = useApi(
+    'project',
+    'update'
+  )
+
+  useEffect(() => {
+    call()
+  }, [])
+
+  const loading = () => loadingUpdate || loadingCreate
+
+  async function onSubmit() {
+    form.validateFields().then(async body => {
+      try {
+        if (value) {
+          callUpdate()
+        } else {
+          console.log(body)
+          callCreate()
+        }
+        onClose()
+      } catch (error) {}
+    })
   }
+
+  const onClose = () => {
+    onCancel()
+    form.resetFields()
+  }
+
   return (
-    <Modal
-      title={project ? 'Edit Project' : 'Add Project'}
+    <Drawer
+      title={value ? 'Edit Project' : 'Add Project'}
       open={open}
-      onOk={onOk}
-      onCancel={onCancel}
+      onClose={onClose}
+      closable={false}
+      mask={false}
+      footer={
+        <Space>
+          <Button
+            onClick={onSubmit}
+            type="primary"
+            htmlType="submit"
+            loading={loading()}
+          >
+            Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </Space>
+      }
+      style={{ borderRadius: '16px', position: 'relative' }}
+      extra={
+        <Button
+          onClick={onClose}
+          size="small"
+          type="text"
+          icon={<CloseOutlined />}
+        />
+      }
     >
-      <Space direction="vertical">
-        <Input
-          placeholder="Project name"
-          value={name}
-          onChange={e => {
-            setName(e.target.value)
-          }}
-        />
-        <Select
-          defaultValue="no-client"
-          style={{ width: 120 }}
-          onChange={e => setClient(e)}
+      <Form
+        form={form}
+        name="basic"
+        requiredMark={false}
+        layout="vertical"
+        validateTrigger="onBlur"
+        style={{ width: '100%' }}
+        initialValues={
+          value
+            ? ProjectUpdateBody.create({
+                abbr: value.abbr,
+                name: value.name,
+                clientId: value.client.id,
+                active: value.active,
+                startDate: value.startDate,
+              })
+            : ProjectCreateBody.createPartially({
+                name: '',
+                abbr: '',
+                startDate: new Date(),
+              })
+        }
+      >
+        <Form.Item
+          name="abbr"
+          label="Abbreviation"
+          rules={[
+            {
+              validator: Project.validator('abbr'),
+              message: 'Please enter a value',
+            },
+          ]}
         >
-          <Select.Option value="no-client">No client</Select.Option>
-          <Select.Option value="a101">A101</Select.Option>
-          <Select.Option value="bim">BİM</Select.Option>
-          <Select.Option value="sok">ŞOK</Select.Option>
-        </Select>
-        <DatePicker
-          allowClear={false}
-          value={startDate}
-          onChange={e => setStartDate(e ? e : dayjs())}
-        />
-      </Space>
-    </Modal>
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              validator: Project.validator('name'),
+              message: 'Please enter a value',
+            },
+          ]}
+        >
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+        <Form.Item
+          rules={[
+            {
+              required: true,
+              message: 'Please select a client',
+            },
+          ]}
+          name="clientId"
+          label="Client"
+        >
+          <Select
+            options={data?.map(e => ({
+              value: e.id,
+              label: `${e.abbr} - ${e.name}`,
+            }))}
+          />
+        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item
+              name="startDate"
+              label="Start date"
+              getValueFromEvent={date => momentToDate(date)}
+              getValueProps={i => ({ value: moment(i) })}
+            >
+              <DatePicker
+                allowClear={false}
+                format={DEFAULT_DATE_FORMAT}
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Drawer>
   )
 }
