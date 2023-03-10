6a08bfc89a44d3809b977e37f0f7691397eb51cb
dogkan-spiky <dogkan@spiky.ai>
2023-03-11 02:51:19 +0300

    refactor: Move rangePresets to local constants file

--- src/pages/reports/components/ReportsFilter.tsx
+++ src/pages/reports/components/ReportsFilter.tsx
@@ -5,26 +5,13 @@ import DropdownAutocomplete from '@/pages/reports/components/DropdownAutocomplet
 import DropdownActivator from '@/components/DropdownActivator'
 import useApi from '@/services/useApi'
 import { useEffect } from 'react'
+import rangePresets from '@/pages/reports/components/constants'
 
 interface RenderProps {
   getReport: (values: any) => void
 }
 
 export default function ReportsFilter({ getReport }: RenderProps) {
-  const rangePresets: {
-    label: string
-    value: [Dayjs, Dayjs]
-  }[] = [
-    { label: 'Today', value: [dayjs(), dayjs()] },
-    { label: 'Yesterday', value: [dayjs().add(-1, 'd'), dayjs()] },
-    { label: 'This week', value: [dayjs().add(-7, 'd'), dayjs()] },
-    { label: 'Last week', value: [dayjs().add(-7, 'd'), dayjs()] },
-    { label: 'Past 2 weeks', value: [dayjs().add(-7, 'd'), dayjs()] },
-    { label: 'This month', value: [dayjs().add(-14, 'd'), dayjs()] },
-    { label: 'Last month', value: [dayjs().add(-30, 'd'), dayjs()] },
-    { label: 'This year', value: [dayjs().add(-90, 'd'), dayjs()] },
-    { label: 'Last year', value: [dayjs().add(-90, 'd'), dayjs()] },
-  ]
   const {
     data: clients,
     call: getAllClients,
---
+++ src/pages/reports/components/constants.ts
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs'
+import type { Dayjs } from 'dayjs'
+
+const rangePresets: {
+  label: string
+  value: [Dayjs, Dayjs]
+}[] = [
+  { label: 'Today', value: [dayjs(), dayjs()] },
+  { label: 'Yesterday', value: [dayjs().add(-1, 'd'), dayjs()] },
+  { label: 'This week', value: [dayjs().add(-7, 'd'), dayjs()] },
+  { label: 'Last week', value: [dayjs().add(-7, 'd'), dayjs()] },
+  { label: 'Past 2 weeks', value: [dayjs().add(-7, 'd'), dayjs()] },
+  { label: 'This month', value: [dayjs().add(-14, 'd'), dayjs()] },
+  { label: 'Last month', value: [dayjs().add(-30, 'd'), dayjs()] },
+  { label: 'This year', value: [dayjs().add(-90, 'd'), dayjs()] },
+  { label: 'Last year', value: [dayjs().add(-90, 'd'), dayjs()] },
+]
+
+export default rangePresets
