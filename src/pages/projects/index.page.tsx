c8d0c5cf44ad44bff2cd5e94520d2d653df916e9
dogkan-spiky <dogkan@spiky.ai>
2023-03-20 17:57:28 +0300

    FE-27 feat: Sort projects by Client

--- src/pages/projects/index.page.tsx
+++ src/pages/projects/index.page.tsx
@@ -55,6 +55,7 @@ export default function ProjectsPage() {
           </Tag>
         )
       },
+      sorter: (a, b) => a.client.abbr.localeCompare(b.client.abbr),
     },
     {
       title: 'Start Date',
