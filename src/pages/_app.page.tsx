a55ae688396cc1f7affa9f89dde415cf0b85e72f
dogkansaracedu <dogkan@spiky.ai>
2023-06-04 16:55:12 +0300

    conf: Set dayjs locale based on lang

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,18 @@
 import 'antd/dist/reset.css'
 import '@/styles/globals.css'
 import { ConfigProvider } from 'antd'
+import * as dayjs from 'dayjs'
 import { appWithTranslation, useTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import tr from 'antd/locale/tr_TR'
 import en from 'antd/locale/en_US'
+import 'dayjs/locale/tr'
+import updateLocale from 'dayjs/plugin/updateLocale'
 import type { Locale } from 'antd/es/locale'
 import { Language } from 'models'
 import TaasLayout from '@/layouts/TaasLayout'
+dayjs.extend(updateLocale)
 
 const App = ({ Component, pageProps }: AppProps) => {
   const antdLocales: { [key in Language]: Locale } = { en, tr } as const
@@ -16,6 +20,9 @@ const App = ({ Component, pageProps }: AppProps) => {
     i18n: { language: l },
   } = useTranslation()
   const lang = l as Language
+  dayjs.updateLocale(lang, {
+    weekStart: 1,
+  })
   return (
     <>
       <Head>
