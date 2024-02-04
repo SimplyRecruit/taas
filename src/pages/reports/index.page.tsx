246a9b0169d38d436951b8d1bd3b04103dec27ee
dogkansaracedu <dogkansarac@outlook.com>
2024-02-04 20:21:35 +0300

    feat: show total times in chart

--- src/pages/reports/index.page.tsx
+++ src/pages/reports/index.page.tsx
@@ -7,7 +7,7 @@ import type { ColumnConfig } from '@ant-design/plots/es/components/column'
 import useApi from '@/services/useApi'
 import { ReportReqBody } from 'models'
 import ReportsFilter from '@/pages/reports/components/ReportsFilter'
-import { Card, Space, Spin } from 'antd'
+import { Card, Spin } from 'antd'
 import { baseConfig } from '@/pages/reports/components/constants'
 import { formatDate } from '@/util'
 import { useTranslation } from 'next-i18next'
@@ -22,16 +22,16 @@ export default function ReportsPage() {
 
   const config: ColumnConfig = useMemo(() => {
     const annotations: any = []
-    each(groupBy(data, 'date'), (values, k) => {
+    each(groupBy(data, 'date'), (values, date) => {
       const value = values.reduce((a: any, b: any) => a + b.totalHours, 0)
       annotations.push({
         type: 'text',
-        position: [k, value],
+        position: [formatDate(new Date(date)), value],
         content: `${value}`,
         style: {
           textAlign: 'center',
           fontSize: 14,
-          fill: 'rgba(0,0,0,0.85)',
+          fill: 'rgb(0,0,0)',
         },
         offsetY: -10,
       })
