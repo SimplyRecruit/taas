67215aac8b904b1d9c3be96f921853be25f80089
dogkansaracedu <dogkansarac@outlook.com>
2024-02-04 20:41:22 +0300

    refactor: remove redundant layout parts

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,10 +1,7 @@
 import ProfileMenu from '@/components/ProfileMenu'
-import { HEADER_HEIGHT, ICON_TOP_MARGIN_FIX, SIDER_WIDTH } from '@/constants'
-import { QuestionCircleOutlined } from '@ant-design/icons'
-import { Badge, Button, Layout, Menu, Space, Typography } from 'antd'
+import { HEADER_HEIGHT, SIDER_WIDTH } from '@/constants'
+import { Layout, Menu, Space, Typography } from 'antd'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { FiBell } from 'react-icons/fi'
 import { adminMenuItems, topMenuItems } from './menu-items'
 import Link from 'next/link'
 import { useTranslation } from 'next-i18next'
@@ -14,6 +11,7 @@ type MenuItem = {
   icon: JSX.Element
   label: string
   path: string
+  type?: string
 }
 
 interface AppLayoutProps {
@@ -23,7 +21,15 @@ interface AppLayoutProps {
 export default function AppLayout({ children }: AppLayoutProps) {
   const { t } = useTranslation('common')
   const router = useRouter()
-  const [sidebarCollapsed] = useState(false)
+
+  const adminItems = [
+    {
+      label: '‏ ‏ ‏' + t('navigationMenu.manage'),
+      key: 'admin',
+      children: generateMenuItems(adminMenuItems),
+      type: 'group',
+    },
+  ]
 
   return (
     <Layout>
@@ -63,26 +69,6 @@ export default function AppLayout({ children }: AppLayoutProps) {
             </Typography.Title>
           </div>
           <Space size="large" align="center">
-            {/* <Button
-              icon={<QuestionCircleOutlined />}
-              target="_blank"
-              type="text"
-            >
-              Help
-            </Button>
-            <div style={{ marginTop: ICON_TOP_MARGIN_FIX }}>
-              <Badge
-                color="red"
-                count={9}
-                style={{
-                  cursor: 'pointer',
-                }}
-                size="small"
-                offset={[1, -1]}
-              >
-                <FiBell size={18} />
-              </Badge>
-            </div> */}
             <ProfileMenu />
           </Space>
         </div>
@@ -92,33 +78,12 @@ export default function AppLayout({ children }: AppLayoutProps) {
           height: `calc(100vh - ${HEADER_HEIGHT}px)`,
         }}
       >
-        <Layout.Sider
-          collapsible
-          collapsed={sidebarCollapsed}
-          theme="light"
-          trigger={null}
-          width={SIDER_WIDTH}
-        >
-          <div style={{ height: '100%' }}>
-            <Menu
-              items={generateMenuItems(topMenuItems)}
-              mode="inline"
-              selectedKeys={[router.pathname.split('/')[1]]}
-            />
-            <Menu
-              style={{ height: '100%' }}
-              items={[
-                {
-                  label: '‏ ‏ ‏' + t('navigationMenu.manage'),
-                  key: 'admin',
-                  children: generateMenuItems(adminMenuItems),
-                  type: 'group',
-                },
-              ]}
-              mode="inline"
-              selectedKeys={[router.pathname.split('/')[1]]}
-            />
-          </div>
+        <Layout.Sider theme="light" width={SIDER_WIDTH}>
+          <Menu
+            items={[...generateMenuItems(topMenuItems), ...adminItems]}
+            mode="inline"
+            selectedKeys={[router.pathname.split('/')[1]]}
+          />
         </Layout.Sider>
         {/* To put scrollbar inside the content */}
         <Layout.Content style={{ overflowY: 'auto', padding: 20 }}>
